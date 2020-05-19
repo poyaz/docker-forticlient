c4fbc31f7fd3eb61fae9d03921da5b94e6b5ddd0
pooya <pooya_azarpour@yahoo.com>
2020-05-20 04:20:20 +0430

    Fix variable name

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-if [ -z "$VPNـADDR" -o -z "$VPNـUSER" -o -z "$VPNـPASS" ]; then
-  echo "Variables VPNـADDR, VPNـUSER and VPNـPASS must be set."
+if [ -z "$VPN_ADDR" -o -z "$VPN_USER" -o -z "$VPN_PASS" ]; then
+  echo "Variables VPN_ADDR, VPN_USER and VPN_PASS must be set."
   exit 1
 fi
 
-export VPNـTIMEOUT=${VPNـTIMEOUT:-5}
+export VPN_TIMEOUT=${VPN_TIMEOUT:-5}
 
 # Setup masquerade, to allow using the container as a gateway
 for iface in $(ip a | grep eth | grep inet | awk '{print $2}'); do
